8ad9cc0e1fd8e0c3d767b175fb30666c9b114b65
Atul Yadav <atulyadav.875435@gmail.com>
2026-08-16 09:38:50 +0530

    ABCD

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    function Signup(Request $req){
+        $input = $req->all();
+        $input['password'] = bcrypt( $input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('MYWEBAPP')->plainTextToken;
+        $user['name']= $user->name;
+        return ['success'=>true , 'result'=>$success , 'msg'=> 'User Register Successfully'];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -28,3 +29,5 @@ Route::get('Search/{name}',[StudentController::class , 'Search']);
 
 
 Route::resource('member',ResourceController::class);
+
+Route::post('Signup',[AuthController::class,'Signup']);
